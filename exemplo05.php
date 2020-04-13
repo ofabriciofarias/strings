10e2c2596bb953a9cde2fef780c5443c80fe4f62
ofabriciofarias <fabriciosf@ufpa.br>
2020-04-12 21:10:58 -0300

    include e require
    Novos materiais que tratam sobre include e require

--- exemplo05.php
+++ exemplo05.php
@@ -1,11 +1,23 @@
 <?php
+/*
 
-include "inc/exemplo04.php";
+include tenta funcionar mesmo que o arquivo não exista,
+o include é mais completo. Procura na pasta e depois procura
+no diretório padrão, familiar ao C.
+
+require obriga a existência do arquivo
+e que funcione de forma correta
+*/
+//include "inc/exemplo04.php";
+require_once "inc/exemplo04.php";
+require_once "inc/exemplo04.php";
 
 $a = 10;
 $b = 25;
 
 $resultado = somar($a, $b);
+
+echo "<br>";
 
 echo $resultado;
 
--- inc/exemplo04.php
+++ inc/exemplo04.php
@@ -1,5 +1,7 @@
 <?php
 
+var_dump("Ok");
+
 function somar($a, $b){
 	return $a + $b;
 }
